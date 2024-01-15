e0f8ca61aa990a136e9ed0be49c6f3b75414ff8d
HenryNg101 <hungnguyenquoc8a2@gmail.com>
2024-01-16 03:45:10 +1030

    Optimize the code

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,30 +1,41 @@
 import React from "react";
 
 export default function About(){
+    const pageContent = [
+        {
+            heading: "What is competitive programming anyway?",
+            paragraphs: `Competitive programming is a mind sport usually held over the Internet or a local network, involving participants trying to program according to provided specifications (totally not copied from Wikipedia).
+            In essence, it is about solving ridiculously difficult problems within unfairly short time limits (because why not).`
+        },
+        {
+            heading: "Why is it even a thing?",
+            paragraphs: `Because it's fun! Ask anyone who has done these contests - they are addicting and fun.
+            Employment: Most competitions are sponsored by multinational software companies such as Google, Microsoft, IBM etc. Each year, they take students who performed well in the contest and offer them positions.
+            Love of problem solving: The problems that you will see are posed as real-world problems, and often will mask the underlying question. You will need to use many ideas that you have learnt in order to solve them.`
+        },
+        {
+            heading: "Tell me about the club already!",
+            paragraphs: `Alright alright, calm down, I hear ya.
+            The Competitive Programming Club is a not-for-profit, student-run club dedicated to promote and nurture the sport of (you guessed it) 'Competitive Programming' at The University of Adelaide.
+            We host practice competitions and training workshops where we solve problems, discuss algorithms, cry over the parts we didn't understand, figure things out, and celebrate with free food.
+            If you think you're the next Larry Page, this is not the club for you. If you want to be the next Larry Page, then this club maybe what you are looking for.
+            All skill levels are welcome because, let's be honest, everyone is going to struggle.`
+        }
+    ]
+
     return (
     <>
-        <h2>What is competitive programming anyway?</h2>
-        Competitive programming is a mind sport usually held over the Internet or a local network, involving participants trying to program according to provided specifications (totally not copied from Wikipedia).
-        <br/><br/>
-        In essence, it is about solving ridiculously difficult problems within unfairly short time limits (because why not).
-        <br/><br/><br/>
-        <h2>Why is it even a thing?</h2>
-        Because it’s fun! Ask anyone who has done these contests - they are addicting and fun.
-        <br/><br/>
-        Employment: Most competitions are sponsored by multinational software companies such as Google, Microsoft, IBM etc. Each year, they take students who performed well in the contest and offer them positions.
-        <br/><br/>
-        Love of problem solving: The problems that you will see are posed as real-world problems, and often will mask the underlying question. You will need to use many ideas that you have learnt in order to solve them.
-        <br/><br/><br/>
-        <h2>Tell me about the club already!</h2>
-        <b>Alright alright, calm down, I hear ya.</b>
-        <br/><br/>
-        The Competitive Programming Club is a not-for-profit, student-run club dedicated to promote and nurture the sport of (you guessed it) ‘Competitive Programming’ at The University of Adelaide.
-        <br/><br/>
-        We host practice competitions and training workshops where we solve problems, discuss algorithms, cry over the parts we didn’t understand, figure things out, and celebrate with free food.
-        <br/><br/>
-        If you think you’re the next Larry Page, this is not the club for you. If you want to be the next Larry Page, then this club maybe what you are looking for.
-        <br/><br/>
-        All skill levels are welcome because, let’s be honest, everyone is going to struggle.
+        {pageContent.map((section, section_id) => (
+            <React.Fragment key={section_id}>
+                <h2>{section.heading}</h2>
+
+                {section.paragraphs.split('\n').map((content, id) => 
+                    <p key={`${section_id} ${id}`} style={{marginBottom: '1.5em'}}>{content}</p>
+                )}
+                
+                <br/>
+            </React.Fragment>
+        ))}
     </>
     );
 }
